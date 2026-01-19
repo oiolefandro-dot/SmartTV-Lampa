87e2e833b5f66fcde41a5c5dd13c17cca0bf816a
oiolefandro-dot <oiolefandro@gmail.com>
2026-01-19 14:15:13 +0500

    Update SmartTVv1.js

--- SmartTVv1.js
+++ SmartTVv1.js
@@ -15,47 +15,14 @@
                     { name: 'Первый HD', url: 'https://cdn10.1internet.tv/dash-live12/streams/1tv/1tvdash.mpd', logo: 'http://epg.it999.ru/img2/983.png' },
                     { name: 'Россия HD', url: 'http://cdnmg.secure.live.rtr-vesti.ru/hls2/russia_hd/playlist_6.m3u8', logo: 'http://epg.it999.ru/img2/984.png' },
                     { name: 'НТВ HD', url: 'https://ntv-hls-orig.cdnvideo.ru/ntv0_hd/tracks-v1a1/mono.m3u8', logo: 'http://epg.it999.ru/img2/2001.png' },
-                    { name: 'ТВ Центр HD', url: 'http://hls.peers.tv/streaming/tvc_hd/126/copy/playlist.m3u8?token=d680c0c502834299a358e0c1783e9f94&client=97', logo: 'http://epg.it999.ru/img2/649.png' },
-                    { name: 'РЕН ТВ', url: 'http://flu.ttk.mx:9090/REN_TV/index.m3u8?token=orh6o', logo: 'http://epg.it999.ru/img2/18.png' },
-                    { name: 'Мир HD', url: 'https://hls-mirtv.cdnvideo.ru/mirtv-parampublish/mirtv_2500/tracks-v1a1/mono.m3u8', logo: 'http://epg.it999.ru/img2/726.png' },
-                    { name: 'Звезда HD', url: 'https://tvchannelstream1.tvzvezda.ru/cdn/tvzvezda/playlist_hdhigh.m3u8', logo: 'http://epg.it999.ru/img2/405.png' },
-                    { name: 'СТС', url: 'http://flu.ttk.mx:9090/STS/index.m3u8?token=orh6o', logo: 'http://epg.it999.ru/img2/79.png' },
-                    { name: 'Домашний', url: 'http://flu.ttk.mx:9090/Domashni/index.m3u8?token=orh6o', logo: 'http://epg.it999.ru/img2/304.png' },
-                    { name: 'Ю', url: 'http://flu.ttk.mx:9090/U/index.m3u8?token=orh6o', logo: 'http://epg.it999.ru/img2/898.png' }
-                ]
-            },
-            {
-                title: '🌏 Познавательные',
-                channels: [
-                    { name: 'Discovery HD', url: 'https://livetv.mylifeisgood.ml/channels/discovery.m3u8', logo: 'http://epg.it999.ru/img2/1612.png' },
-                    { name: 'Animal Planet HD', url: 'https://livetv.mylifeisgood.ml/channels/animalplanethd', logo: 'http://epg.it999.ru/img2/990.png' },
-                    { name: 'Моя планета', url: 'http://cdnmg.secure.live.rtr-vesti.ru/live/smil:mplan.smil/chunklist_b1600000.m3u8', logo: 'http://epg.it999.ru/img2/675.png' },
-                    { name: 'Discovery Science HD', url: 'https://livetv.mylifeisgood.ml/channels/discoveryscience.m3u8', logo: 'http://epg.it999.ru/img2/3067.png' }
-                ]
-            },
-            {
-                title: '⛹️ Спортивные',
-                channels: [
-                    { name: 'SETANTA SPORTS 1 HD', url: 'https://livetv.mylifeisgood.ml/channels/setantahd', logo: 'http://epg.it999.ru/img2/2812.png' },
-                    { name: 'Eurosport 1 HD', url: 'https://livetv.mylifeisgood.ml/channels/eurosport1ru', logo: 'http://epg.it999.ru/img2/2052.png' },
-                    { name: 'Телеканал Футбол', url: 'http://flu.ttk.mx:9090/Futbol/index.m3u8?token=orh6o', logo: 'http://epg.it999.ru/img2/2782.png' }
+                    { name: 'СТС', url: 'http://flu.ttk.mx:9090/STS/index.m3u8?token=orh6o', logo: 'http://epg.it999.ru/img2/79.png' }
                 ]
             },
             {
                 title: '📽️ Фильмовые',
                 channels: [
                     { name: 'Hollywood HD', url: 'http://46.19.100.174:4450/play/a005/index.m3u8', logo: 'http://epg.it999.ru/img2/934.png' },
-                    { name: 'VIP Premiere HD', url: 'https://bl.uma.media/live/485536/HLS/4614144_3/2/1/playlist.m3u8', logo: 'http://epg.it999.ru/img2/2822.png' },
-                    { name: 'TV 1000 HD', url: 'https://bl.uma.media/live/485542/HLS/4614144_3/2/1/playlist.m3u8', logo: 'http://epg.it999.ru/img2/3302.png' },
-                    { name: 'Кино ТВ HD', url: 'https://livetv.mylifeisgood.ml/channels/kinotvst.m3u8', logo: 'http://epg.it999.ru/img2/2506.png' }
-                ]
-            },
-            {
-                title: '🎵 Музыкальные',
-                channels: [
-                    { name: 'Муз ТВ', url: 'http://zabava-htlive.cdn.ngenix.net/hls/CH_MUZTV/variant.m3u8', logo: 'http://epg.it999.ru/img2/897.png' },
-                    { name: 'RU.TV', url: 'https://hls-03-video.webcaramba.com/rutv/live_720/index.m3u8', logo: 'http://epg.it999.ru/img/2277.png' },
-                    { name: 'Шансон-ТВ', url: 'http://hls-shansontv.cdnvideo.ru/shansontv/shansontv.sdp/playlist.m3u8', logo: 'https://epg.iptvx.one/icn/shanson-tv.png' }
+                    { name: 'VIP Premiere HD', url: 'https://bl.uma.media/live/485536/HLS/4614144_3/2/1/playlist.m3u8', logo: 'http://epg.it999.ru/img2/2822.png' }
                 ]
             }
         ];
@@ -65,7 +32,7 @@
             this.activity.loader(false);
             
             data_groups.forEach(function(group) {
-                var line = $('<div class="items-line"><div class="items-line__title" style="margin: 1.5em 0 0.5em 1.2em; font-size: 1.4em; color: #fff; font-weight: 500;">'+group.title+'</div><div class="items-line__body"></div></div>');
+                var line = $('<div class="items-line"><div class="items-line__title" style="margin: 1.5em 0 0.5em 1.2em; font-size: 1.4em; color: #fff;">'+group.title+'</div><div class="items-line__body"></div></div>');
                 
                 group.channels.forEach(function(channel) {
                     var item = Lampa.Template.get('card', {title: channel.name, release_year: ''});
@@ -73,9 +40,7 @@
                     item.find('.card__view, .card__age, .card__type').remove();
                     
                     item.on('hover:enter', function () {
-                        var video = { title: channel.name, url: channel.url };
-                        Lampa.Player.play(video);
-                        Lampa.Player.playlist([video]);
+                        Lampa.Player.play({ title: channel.name, url: channel.url });
                     });
                     
                     line.find('.items-line__body').append(item);
@@ -92,38 +57,33 @@
         this.destroy = function () {
             network.clear();
             scroll.destroy();
-            html.remove();
-            body.remove();
             items = [];
         };
     }
 
-    function startPlugin() {
-        if (!window.smart_tv_plugin_installed) {
-            window.smart_tv_plugin_installed = true;
-            Lampa.Component.add('smart_tv', SmartTV_Plugin);
+    // РЕГИСТРАЦИЯ ПЛАГИНА
+    if (window.Lampa) {
+        Lampa.Component.add('smart_tv', SmartTV_Plugin);
 
-            var addMenuItem = function() {
-                var menu = $('.menu .menu__list');
-                if (menu.length > 0 && !$('.menu__item[data-action="smart_tv"]').length) {
-                    var menu_item = $('<li class="menu__item selector" data-action="smart_tv">' +
-                        '<div class="menu__ico"><svg width="36" height="36" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M21 7L12 3L3 7V17L12 21L21 17V7Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></div>' +
-                        '<div class="menu__text">Smart TV</div>' +
-                        '</li>');
+        // 1. Добавляем в настройки, чтобы вы могли запустить его оттуда для проверки
+        Lampa.SettingsApi.addPart({
+            component: 'smart_tv',
+            name: 'Smart TV',
+            icon: '<svg width="36" height="36" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M21 7L12 3L3 7V17L12 21L21 17V7Z" stroke="currentColor" stroke-width="2"/></svg>'
+        });
 
-                    menu_item.on('hover:enter', function () {
-                        Lampa.Activity.push({ title: 'Smart TV', component: 'smart_tv', page: 1 });
-                    });
-                    menu.append(menu_item);
-                }
-            };
-
-            addMenuItem();
-            var timer = setInterval(addMenuItem, 2000);
-            setTimeout(function() { clearInterval(timer); }, 15000);
+        // 2. Добавляем в основное меню (улучшенный поиск меню)
+        var addBtn = function() {
+            var menu = $('.menu__list');
+            if (menu.length && !$('.menu__item[data-action="smart_tv"]').length) {
+                var btn = $('<li class="menu__item selector" data-action="smart_tv"><div class="menu__ico"><svg width="36" height="36" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M21 7L12 3L3 7V17L12 21L21 17V7Z" stroke="currentColor" stroke-width="2"/></svg></div><div class="menu__text">Smart TV</div></li>');
+                btn.on('hover:enter', function() {
+                    Lampa.Activity.push({ title: 'Smart TV', component: 'smart_tv' });
+                });
+                menu.append(btn);
+            }
         }
+        
+        setInterval(addBtn, 2000);
     }
-
-    if (window.appready) startPlugin();
-    else Lampa.Listener.follow('app', function (e) { if (e.type == 'ready') startPlugin(); });
 })();
